95577c329a79635de1128ee69fc8990db9490ecd
Sudarev <soul7341@gmail.com>
2023-10-22 20:36:52 +0300

    Added interfaces and class Worker

--- src/Worker.java
+++ src/Worker.java
@@ -2,14 +2,14 @@ public class Worker {
     private OnTaskDoneListener callback;
     private OnTaskErrorListener errorCallback;
 
-    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback){
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
         this.callback = callback;
         this.errorCallback = errorCallback;
     }
 
     public void start() {
         for (int i = 0; i < 100; i++) {
-            if (i==33) errorCallback.onError("Task "+i+" was failed");
+            if (i == 33) errorCallback.onError("Task " + i + " was failed");
             callback.onDone("Task " + i + " is done");
         }
     }
